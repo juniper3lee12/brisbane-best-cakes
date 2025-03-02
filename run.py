deec939390183c50a637d7ed20d575cc4f19b788
juniper3lee12 <lastgiggoo@gmail.com>
2025-03-02 12:49:58 +1000

    revised run.py

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
+import os
 from BestCakesinBrisbane import create_app
 
 if __name__=='__main__':
     napp=create_app()
-    napp.run(host='0.0.0.0', port=5000)
+    napp.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
